95c552eee89143cb2f0e688d89af406b38b0c0bc
Jake Teton-Landis <just.1.jake@gmail.com>
2020-01-26 16:38:46 -0800

    switch build to webpack for deps

---
+++ webpack.config.js
@@ -0,0 +1,21 @@
+module.exports = {
+  mode: 'development',
+  entry: './script.ts',
+  devtool: 'cheap-module-eval-source-map',
+  module: {
+    rules: [
+      {
+        test: /\.tsx?$/,
+        use: 'ts-loader',
+        exclude: /node_modules/,
+      },
+    ],
+  },
+  resolve: {
+    extensions: [ '.tsx', '.ts', '.js' ],
+  },
+  output: {
+    filename: 'script.js',
+    path: __dirname,
+  },
+};
